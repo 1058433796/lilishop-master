926a56a720874e6aa83a476207868c5a727a4d0d
1058433796 <1058433796@qq.com>
2022-09-22 22:40:48 +0800

    重新引用包，消除报错

--- seller-api/src/main/java/cn/lili/controller/itemOrder/itemOrderController.java
+++ seller-api/src/main/java/cn/lili/controller/itemOrder/itemOrderController.java
@@ -1,10 +1,9 @@
 package cn.lili.controller.itemOrder;
 
 import cn.lili.common.enums.ResultUtil;
-import cn.lili.common.security.context.UserContext;
 import cn.lili.common.vo.ResultMessage;
-import cn.lili.modules.itemOrder.entity.ItemOrder;
-import cn.lili.modules.itemOrder.entity.ItemOrderSearchParams;
+import cn.lili.modules.itemOrder.entity.dto.ItemOrderSearchParams;
+import cn.lili.modules.itemOrder.entity.vo.ItemOrderSimpleVO;
 import cn.lili.modules.itemOrder.service.ItemOrderService;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.annotations.ApiOperation;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/store/itemOrder/itemOrder")
@@ -23,7 +21,7 @@ public class itemOrderController {
 
     @ApiOperation(value = "分页获取项目列表")
     @GetMapping("/list")
-    public ResultMessage<IPage<ItemOrder>> getByPage(ItemOrderSearchParams itemOrderSearchParams) {
+    public ResultMessage<IPage<ItemOrderSimpleVO>> getByPage(ItemOrderSearchParams itemOrderSearchParams) {
         //获取当前登录商家账号
 //        String storeId = Objects.requireNonNull(UserContext.getCurrentUser()).getStoreId();
 //        itemOrderSearchParams.setBuyerId(storeId);
