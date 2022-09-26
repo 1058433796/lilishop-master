3299b3c0377547a283e3f5cf36c309eb5b6ec5b9
ghy <1159069190@qq.com>
2022-09-26 15:42:39 +0800

    Revert "itemOrder和OrderGood实体增加属性"
    This reverts commit 61227fcf31a5a3ce2885f41895b652b574c03c2b.

--- framework/src/main/java/cn/lili/modules/itemOrder/entity/dos/ItemOrder.java
+++ framework/src/main/java/cn/lili/modules/itemOrder/entity/dos/ItemOrder.java
@@ -57,19 +57,16 @@ public class ItemOrder
     private String buyerPhone;
 
     @ApiModelProperty(value = "收货人姓名")
-    private String consigneeName;
-
-    @ApiModelProperty(value = "收货人地址")
-    private String buyerAddress;
+    private String buyerName;
 
     @ApiModelProperty(value = "订单金额")
     private Double orderAmount;
 
+    @ApiModelProperty(value = "收货人地址")
+    private String buyerAddress;
+
     @ApiModelProperty(value = "供应商响应状态")
     private String storeReply;
-
-    @ApiModelProperty(value = "采购方响应状态")
-    private String buyerReply;
 
     @ApiModelProperty(value = "合同合同签署状态")
     private String contractStatus;
@@ -91,14 +88,9 @@ public class ItemOrder
     @ApiModelProperty(value = "采购方ID")
     private String buyerId;
 
-    @ApiModelProperty(value = "采购商名称")
-    private String buyerName;
-
     @ApiModelProperty(value = "供应商ID")
     private String storeId;
 
-    @ApiModelProperty(value = "供应商名称")
-    private String storeName;
 
 
 
--- framework/src/main/java/cn/lili/modules/itemOrder/entity/dos/OrderGood.java
+++ framework/src/main/java/cn/lili/modules/itemOrder/entity/dos/OrderGood.java
@@ -22,9 +22,6 @@ public class OrderGood
     @ApiModelProperty("订单编号")
     private String orderId;
 
-    @ApiModelProperty("子订单编号")
-    private String subId;
-
     @ApiModelProperty(value = "品名")
     private String goodName;
 
