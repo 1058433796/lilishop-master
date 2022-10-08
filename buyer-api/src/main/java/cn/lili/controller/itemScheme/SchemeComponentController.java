ad63cd194014449d69126bf16117e675194ecf33
chuanranchuanran <34791646+chuanranchuanran@users.noreply.github.com>
2022-10-08 10:19:59 +0800

    lzy区分采购方、供应商api，部分framework修改

---
+++ buyer-api/src/main/java/cn/lili/controller/itemScheme/SchemeComponentController.java
@@ -0,0 +1,123 @@
+package cn.lili.controller.itemScheme;
+
+import cn.hutool.core.date.DateTime;
+import cn.lili.Snowflake;
+import cn.lili.common.enums.ResultUtil;
+import cn.lili.common.vo.ResultMessage;
+import cn.lili.modules.item.entity.ItemScheme;
+import cn.lili.modules.item.service.ItemSchemeService;
+import cn.lili.modules.itemOrder.entity.ItemOrder;
+import cn.lili.modules.itemOrder.service.ItemOrderServiceZy;
+import cn.lili.modules.schemeComponent.entity.SchemeComponent;
+import cn.lili.modules.schemeComponent.entity.SchemeComponentSearchParams;
+import cn.lili.modules.schemeComponent.mapper.IdTotal;
+import cn.lili.modules.schemeComponent.service.SchemeComponentService;
+import cn.lili.modules.store.entity.dos.Store;
+import cn.lili.modules.store.entity.dto.StoreAfterSaleAddressDTO;
+import cn.lili.modules.store.entity.vos.StoreVO;
+import cn.lili.modules.store.service.StoreDetailService;
+import cn.lili.modules.store.service.StoreServiceZy;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/store/schemeComponent/schemeComponent")
+public class SchemeComponentController {
+    @Resource
+    private SchemeComponentService SchemeComponentService;
+
+    @Autowired
+    StoreServiceZy storeServiceZy;
+
+    @Autowired
+    ItemOrderServiceZy itemOrderServiceZy;
+
+    @Autowired
+    StoreDetailService storeDetailService;
+    @Autowired
+    ItemSchemeService itemSchemeService;
+    Logger logger = LoggerFactory.getLogger(SchemeComponentController.class);
+
+    @ApiOperation(value = "分页获取方案零件列表")
+    @GetMapping("/list")
+    public ResultMessage<IPage<SchemeComponent>> getByPage(SchemeComponentSearchParams schemeComponentSearchParams) {
+        return ResultUtil.data(SchemeComponentService.queryByParams(schemeComponentSearchParams));
+    }
+
+    @PostMapping(value = "/{id}/supplier")
+    @ApiOperation(value = "通过方案编号创建订单")
+    public  ResultMessage<Object> getSchemeSupplier(@PathVariable("id") String schemePrimaryId) {
+        System.out.println("建立订单");
+        ItemScheme itemScheme  = itemSchemeService.getById(schemePrimaryId);
+        String schemeId = itemScheme.getSchemeId();
+        List<IdTotal> supplierList = SchemeComponentService.getOrderBy(schemeId);
+        StoreVO buyerStore = storeServiceZy.getStoreDetail();
+        StoreAfterSaleAddressDTO storep = storeDetailService.getStoreAfterSaleAddressDTO();
+        Snowflake snowflake = Snowflake.getInstanceSnowflake();
+        ArrayList<Object> arrayList = new ArrayList<>();
+        for(IdTotal idTotal : supplierList) {
+            try {
+                ItemOrder itemOrder = new ItemOrder();
+                itemOrder.setOrderId(snowflake.nextId() + "");
+                itemOrder.setOrderAmount(idTotal.getTotal()+"");
+                Store supplierStore = storeServiceZy.getById(idTotal.getId());
+                itemOrder.setStoreName(supplierStore.getStoreName());
+                itemOrder.setStoreId(idTotal.getId());
+                itemOrder.setCreateTime(DateTime.now());
+                itemOrder.setSchemeId(schemePrimaryId);
+                itemOrder.setBuyerPhone(storep.getSalesConsigneeMobile());
+                itemOrder.setBuyerName(storep.getSalesConsigneeName());
+                itemOrder.setBuyerAddress(buyerStore.getStoreAddressPath() + buyerStore.getStoreAddressDetail());
+                itemOrder.setBuyerId(buyerStore.getId());
+                itemOrderServiceZy.save(itemOrder);
+                arrayList.add(itemOrder);
+            }catch (Exception e) {
+                logger.error("Order create error: schemeID:" + schemeId);
+                throw new RuntimeException(e);
+            }
+        }
+//        SchemeComponentSearchParams  schemeComponentSearchParams = new SchemeComponentSearchParams();
+//        schemeComponentSearchParams.setSchemeId(schemeId);
+//        IPage<SchemeComponent> iPage = SchemeComponentService.queryByParams(schemeComponentSearchParams);
+//        List<SchemeComponent> schemeComponents = iPage.getRecords();
+//        ArrayList<Object> arrayList = new ArrayList<>();
+//        Snowflake snowflake = Snowflake.getInstanceSnowflake();
+//        // 获取采购方店铺信息
+//
+//        for (int i=0; i < schemeComponents.size(); ++i) {
+//            SchemeComponent schemeComponent = schemeComponents.get(i);
+//            String ids = schemeComponent.getSupplierId();
+//            Store supplierStore = storeService.getById(ids);
+//            DateTime dateTime = DateTime.now();
+//            ItemOrder itemOrder = new ItemOrder();
+//            itemOrder.setOrderAmount(Double.parseDouble(schemeComponent.getComponentUnitPrice())*Double.parseDouble(schemeComponent.getComponentNumber())+"");
+//            itemOrder.setStoreName(supplierStore.getStoreName());
+//            // 设置订单中与采购方相关的信息
+//            itemOrder.setBuyerPhone(storep.getSalesConsigneeMobile());
+//            itemOrder.setBuyerName(storep.getSalesConsigneeName());
+//            itemOrder.setBuyerAddress(buyerStore.getStoreAddressPath() + buyerStore.getStoreAddressDetail());
+//            itemOrder.setBuyerId(buyerStore.getId());
+//            try {
+//                // 生成订单号、订单产生时间并设置订单所对应的供应商店铺的id
+//                itemOrder.setOrderId(snowflake.nextId()+"");
+//                itemOrder.setCreateTime(dateTime.now());
+//                itemOrder.setStoreId(ids);
+//                itemOrderService.save(itemOrder);
+//                arrayList.add(itemOrder);
+//            } catch (Exception e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+       return ResultUtil.data(arrayList);
+    }
+
+
+}
---
+++ framework/src/main/java/cn/lili/modules/itemOrder/mapper/ItemOrderMapperZy.java
@@ -0,0 +1,56 @@
+package cn.lili.modules.itemOrder.mapper;
+
+import cn.lili.modules.contract.contract.entity.Contract;
+import cn.lili.modules.itemOrder.entity.dos.ItemOrder;
+import cn.lili.modules.itemOrder.entity.vo.OrderWithContractSimpleV0;
+import cn.lili.modules.schemeComponent.entity.SchemeComponent;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface ItemOrderMapperZy extends BaseMapper<ItemOrder> {
+
+
+
+
+
+
+
+
+
+
+
+    @Select("select g.* from order as g")
+    IPage<ItemOrder> queryByParams(IPage<ItemOrder> page, @Param(Constants.WRAPPER) Wrapper<ItemOrder> queryWrapper);
+
+
+
+
+
+
+
+
+
+
+    @Select("SELECT * FROM scheme_component WHERE scheme_id = " +
+            "(SELECT scheme_id FROM item_scheme WHERE primary_id = " +
+            "(SELECT scheme_id FROM item_order WHERE order_id=#{oid})) AND supplier_id=#{storeId}")
+    List<SchemeComponent> queryOrderComponent(String oid, String storeId);
+    @Select("SELECT * FROM item_order WHERE scheme_id=#{oid}")
+    List<ItemOrder> getAssociatedOrders(String oid);
+    @Update("UPDATE item_order SET pay_status='已付款' WHERE order_id=#{oid}")
+    void payOrder(String oid);
+
+    @Select("SELECT * FROM " +
+            "(SELECT * FROM item_order) AS o " +
+            "NATURAL JOIN " +
+            "(SELECT order_id, buyer_state FROM contract) AS c ${ew.customSqlSegment}")
+    IPage<OrderWithContractSimpleV0> queryAssociatedContractOrders(IPage<Contract> page, @Param(Constants.WRAPPER) Wrapper<Contract> queryWrapper);
+
+}
