7fe6061c4f2423842f25c398b88504d3f34e9637
LYJ <1058433796@qq.com>
2022-11-04 16:45:32 +0800

    修改代码

--- seller-api/src/main/java/cn/lili/controller/passport/StorePassportController.java
+++ seller-api/src/main/java/cn/lili/controller/passport/StorePassportController.java
@@ -79,13 +79,13 @@ public class StorePassportController {
     })
     @PostMapping("/storeLogin")
     public ResultMessage<Object> storeLogin(@NotNull(message = "用户名不能为空") @RequestParam String username,
-                                           @NotNull(message = "密码不能为空") @RequestParam String password, @RequestHeader String uuid) {
-            try {
-                Token token = this.memberService.usernameStoreLogin(username, password);
-                return ResultUtil.data(token);
-            }catch (ServiceException e){
-                return ResultUtil.error(e.getResultCode());
-            }
+                                            @NotNull(message = "密码不能为空") @RequestParam String password, @RequestHeader String uuid) {
+        try {
+            Token token = this.memberService.usernameStoreLogin(username, password);
+            return ResultUtil.data(token);
+        }catch (ServiceException e){
+            return ResultUtil.error(e.getResultCode());
+        }
     }
 
     @PostMapping("/userRole")
@@ -109,10 +109,10 @@ public class StorePassportController {
 
     @PostMapping("/userRegister")
     public ResultMessage<Object> userRegister(@NotNull(message = "用户名不能为空") @RequestParam String username,
-                                           @NotNull(message = "密码不能为空") @RequestParam String password,
+                                              @NotNull(message = "密码不能为空") @RequestParam String password,
                                               @NotNull(message = "手机号不能为空") @RequestParam String mobile,
                                               @RequestHeader String uuid
-                                              ) {
+    ) {
         this.memberService.storeRegister(username, password, mobile);
         return ResultUtil.success();
     }
